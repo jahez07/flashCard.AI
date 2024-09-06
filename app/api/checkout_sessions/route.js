b0767d8f1d9e3fe38f461bcf37244bf686fa8333
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-09-06 11:11:52 +0400

    Update route.js

--- app/api/checkout_sessions/route.js
+++ app/api/checkout_sessions/route.js
@@ -1,7 +1,10 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
+const formatAmountForStripe = (amount) => {
+    return Math.round(amount * 100)
+}
 export async function POST(req) {
-    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 }
